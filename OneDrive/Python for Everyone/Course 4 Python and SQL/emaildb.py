2017b6b5c4402aab56b48af9648a5d8def39dbee
Peter A. Van Loon <captvanloonpa@gmail.com>
2020-05-01 09:17:34 -0400

    PY4E Course 4

---
+++ OneDrive/Python for Everyone/Course 4 Python and SQL/emaildb.py
@@ -0,0 +1,41 @@
+import sqlite3
+import re
+
+
+conn = sqlite3.connect('orgcount.sqlite')
+cur = conn.cursor() # like a handle
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    print(email)
+    remainder = re.split(('@'),email)
+    org = remainder[-1]
+    print(org)
+    print(type(org))
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))# ? is a placeholder
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
